f33efa0206d2ce7654c61560a26211847b3367f2
Robotz213 <nicholas@robotz.dev>
2025-06-02 19:35:49 -0300

    Implement JWT authentication for Socket.IO events and refactor namespace handling

--- dusdoc_api/decorators/jwt.py
+++ dusdoc_api/decorators/jwt.py
@@ -1,4 +1,7 @@
 from functools import wraps
+from quart_jwt_extended import decode_token
+from dusdoc_api.app import app
+from socketio import AsyncServer
 
 
 def jwt_socketio_required(func):
@@ -7,16 +10,34 @@ def jwt_socketio_required(func):
     """
 
     @wraps(func)
-    async def wrapper(*args, **kwargs):
-        from quart_jwt_extended.utils import verify_token_claims
+    async def wrapper(
+        self: AsyncServer = None,
+        sid: str = None,
+        data: str | int = None,
+        *args: str | int | AsyncServer,
+        **kwargs: str | int | AsyncServer,
+    ):
+        args = args
+        kwargs = kwargs
+        environ = list(self.server.environ.items())
+        for key, _ in environ:
+            environ = self.server.environ[key]
+            break
 
-        arg = args
-        env = arg[0].server.environ
-        # Verify the JWT in the request
-        jwt_data = kwargs.get("token")
-        verify_token_claims(jwt_data)
+        items = environ.get("QUERY_STRING", "").split("&")
+        key_value_pairs = [item.split("=") for item in items if "=" in item]
+
+        for key, value in key_value_pairs:
+            environ[key] = value
+
+        token = environ.get("token")
+        if not token:
+            return dict(error="Unauthorized", status=401)
+
+        async with app.app_context():
+            decoded_token = decode_token(token)  # noqa: F841
 
         # Call the original function
-        return await func(*args, **kwargs)
+        return await func(self, sid, data, *args, **kwargs)
 
     return wrapper
--- dusdoc_api/socket_dusdoc/namespaces/__init__.py
+++ dusdoc_api/socket_dusdoc/namespaces/__init__.py
@@ -1,11 +1,20 @@
 from socketio import AsyncNamespace
+from quart_jwt_extended import decode_token
+from dusdoc_api.app import app
+from dusdoc_api.decorators.jwt import jwt_socketio_required
 
 session = set()
 
 
 class DusdocNamespace(AsyncNamespace):
-    async def on_get_status_system(self, sid, *args, **kwargs):
+    @jwt_socketio_required
+    async def on_connect(self, sid, environ, *args, **kwargs):
+        """"""
+
+    @jwt_socketio_required
+    async def on_get_status_system(self, sid, data, *args, **kwargs):
         """Handle a request for the status of the server."""
 
+        # If the token is valid, proceed with the request
         await self.emit("status", dict(sid=sid))
         return dict(sid=sid)
