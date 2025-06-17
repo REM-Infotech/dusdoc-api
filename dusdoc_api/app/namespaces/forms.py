d31832f7061e0dc5d0bbc5bb49ace22fc8e08c28
Robotz213 <nicholas@robotz.dev>
2025-06-17 10:43:45 -0300

    ✨ Refactor file handling in FuncionarioFormsNamespace to use socket_data for file uploads

--- dusdoc_api/app/namespaces/forms.py
+++ dusdoc_api/app/namespaces/forms.py
@@ -8,8 +8,23 @@ from quart_socketio import Namespace
 class FuncionarioFormsNamespace(Namespace):
     async def on_admissional_form(self):
         try:
-            file = await request.files
-            for k, v in list(file.items()):
+            data = await request.socket_data
+            for k, v in list(data.items()):
+                v: FileStorage
+                print(v.stream.read())
+
+                parent = Path(__file__).parent.resolve()
+                await v.save(parent.joinpath("test.xlsx"))
+            return True
+
+        except Exception as e:
+            print(e)
+            return False
+
+    async def on_admissional_files(self):
+        try:
+            data = request.socket_data
+            for k, v in list(data.items()):
                 v: FileStorage
                 print(v.stream.read())
 
