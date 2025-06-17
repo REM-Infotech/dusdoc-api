259633d508c6657eeb0a9d223f94ff1358c45956
Robotz213 <nicholas@robotz.dev>
2025-06-17 16:33:11 -0300

    ✨ Refactor forms and admissional routes for improved structure
    dusdoc_api/app/routes/forms/__init__.py: Added imports and structured the forms blueprint for better organization.
    dusdoc_api/app/routes/forms/admissional.py: Introduced AdmissionalForm class with proper initialization and response handling.

--- dusdoc_api/app/routes/forms/__init__.py
+++ dusdoc_api/app/routes/forms/__init__.py
@@ -1,1 +1,15 @@
 # noqa: D104
+
+
+from quart import Blueprint, Quart
+
+from dusdoc_api.models.users.admin import Users
+
+forms = Blueprint("forms", __name__, "/forms")
+
+
+async def register_api(app: Quart) -> None:  # noqa: D103
+    from .admissional import AdmissionalForm
+
+    form_admissional = AdmissionalForm.as_view("form-admissional", Users)
+    forms.add_url_rule("/admissional", view_func=form_admissional)
--- dusdoc_api/app/routes/forms/admissional.py
+++ dusdoc_api/app/routes/forms/admissional.py
@@ -1,1 +1,12 @@
-# noqa: D100
+from quart import jsonify, make_response  # noqa: D100
+from quart.views import MethodView
+
+from dusdoc_api.models.users.admin import Users
+
+
+class AdmissionalForm(MethodView):  # noqa: D101
+    def __init__(self, model: Users) -> None:  # noqa: D107
+        self.model = model
+
+    async def post(self):  # noqa: ANN201
+        return await make_response(jsonify(ok="ok"))
