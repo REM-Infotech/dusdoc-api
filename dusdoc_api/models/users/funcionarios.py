d0dd74d5c0ad1bfc8d29b5b2b4f32f9dcd6a0de3
Robotz213 <nicholas@robotz.dev>
2025-06-24 11:40:35 -0300

    🔧 Refactor funcionarios model attributes for consistency
    dusdoc_api/models/users/funcionarios.py: Standardizes attribute definitions by removing type hints for consistency across the model.

--- dusdoc_api/models/users/funcionarios.py
+++ dusdoc_api/models/users/funcionarios.py
@@ -11,23 +11,23 @@ class Funcionarios(db.Model):  # noqa: D101
     __tablename__ = "funcionarios"
     id = Column(Integer, primary_key=True)
     # Dados de login
-    nome: str = Column(String(length=64))
-    email: str = Column(String(length=64))
-    password: str = Column(String(length=60))
+    nome = Column(String(length=64))
+    email = Column(String(length=64))
+    password = Column(String(length=60))
     data_admissao = Column(DateTime)
     login_time = Column(DateTime)
-    verification_code: str = Column(String(length=45), unique=True)
-    login_id: str = Column(String(length=64))
-    filename: str = Column(String(length=128))
+    verification_code = Column(String(length=45), unique=True)
+    login_id = Column(String(length=64))
+    filename = Column(String(length=128))
     blob_doc = Column(LargeBinary(length=(2**32) - 1))
 
     # Dados de cadastro
-    codigo: str = Column(String(length=6), unique=True)
-    deficiencia: str = Column(String(length=64))
-    cargo: str = Column(String(length=64))
-    departamento: str = Column(String(length=64))
-    empresa: str = Column(String(length=64))
-    cpf: str = Column(String(length=14), unique=True)
+    codigo = Column(String(length=6), unique=True)
+    deficiencia = Column(String(length=64))
+    cargo = Column(String(length=64))
+    departamento = Column(String(length=64))
+    empresa = Column(String(length=64))
+    cpf = Column(String(length=14), unique=True)
 
 
 class Cargos(db.Model):  # noqa: D101
@@ -46,7 +46,7 @@ class Departamento(db.Model):  # noqa: D101
 class Empresa(db.Model):  # noqa: D101
     __tablename__ = "empresa"
     id: int = Column(Integer, primary_key=True)
-    nome_empresa: str = Column(String(length=64), unique=True)
-    cnpj_empresa: str = Column(String(length=64), unique=True)
-    filename: str = Column(String(length=128))
+    nome_empresa = Column(String(length=64), unique=True)
+    cnpj_empresa = Column(String(length=64), unique=True)
+    filename = Column(String(length=128))
     blob_doc: LargeBinary = Column(db.LargeBinary(length=(2**32) - 1))
