c9fbf9933d31a50abad1138b78d9dabbe61d0eba
Robotz213 <nicholas@robotz.dev>
2025-06-26 15:17:43 -0400

    ✨ Enhance AdmissionalFormView to handle file uploads and JSON data
    dusdoc_api/app/domain/funcionarios/routes/forms/admissional.py: Added logic to process file uploads and handle JSON data in the post method, improving data handling capabilities.

--- dusdoc_api/app/domain/funcionarios/routes/forms/admissional.py
+++ dusdoc_api/app/domain/funcionarios/routes/forms/admissional.py
@@ -1,5 +1,9 @@
-from quart import Response, jsonify, make_response  # noqa: D100
+# noqa: D100
+import json
+
+from quart import Response, jsonify, make_response, request
 from quart.views import MethodView
+from werkzeug.datastructures import CombinedMultiDict
 
 from dusdoc_api.models.users.admin import Users
 
@@ -15,4 +19,14 @@ class AdmissionalFormView(MethodView):  # noqa: D101
         return await make_response(jsonify(ok="ok"))
 
     async def post(self) -> Response:  # noqa: ANN003, ANN201
+        files = await request.files
+        data = await request.data or await request.form or await request.json
+        if isinstance(data, bytes):
+            data = data.decode()
+            if isinstance(data, str):
+                data = json.loads(data)
+
+        body_request = CombinedMultiDict([data, files])
+
+        print(body_request)
         return await make_response(jsonify(ok="ok"))
