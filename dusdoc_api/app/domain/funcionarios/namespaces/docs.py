ae6340e2b777da17bb4aa57af290cd9971d8af81
Robotz213 <nicholas@robotz.dev>
2025-06-26 11:07:17 -0400

    ✨ Refactor FuncionarioDocsNamespace and enhance data handling
    dusdoc_api/app/domain/funcionarios/namespaces/docs.py: Introduce RegistroAdmissao TypedDict for structured data handling and refactor on_solicitados method to improve query logic and data return format.

--- dusdoc_api/app/domain/funcionarios/namespaces/docs.py
+++ dusdoc_api/app/domain/funcionarios/namespaces/docs.py
@@ -3,13 +3,21 @@
 # from quart import request
 # from quart.datastructures import FileStorage
 import json
+from datetime import datetime
 from pathlib import Path
+from typing import TypedDict
 
 import aiofiles
 from flask_sqlalchemy import SQLAlchemy
 from quart import current_app, request
 from quart_jwt_extended import jwt_required  # noqa: F401
 from quart_socketio import Namespace
+
+
+class RegistroAdmissao(TypedDict):  # noqa: D101
+    solicitacao_desc: str
+    data_solicitacao: datetime
+    prazo: datetime
 
 
 class FuncionarioDocsNamespace(Namespace):  # noqa: D101
@@ -21,13 +29,31 @@ class FuncionarioDocsNamespace(Namespace):  # noqa: D101
             return data
 
     async def on_solicitados(self) -> list[dict[str, str]]:
+        from dusdoc_api.models.admissional import RegistryAdmissao
         from dusdoc_api.models.users.funcionarios import Funcionarios
 
         data = request.socket_data
         db: SQLAlchemy = current_app.extensions["sqlalchemy"]
-        user = db.session.query(Funcionarios).filter(Funcionarios.id == data["id"]).first()  # noqa: F841
-        path_parent = Path(__file__).cwd().joinpath("dusdoc_api", "examples", "example_solicitados.json")
-        async with aiofiles.open(str(path_parent), "r", encoding="utf-8") as f:
-            readfile = await f.read()
-            data = json.loads(readfile)
-            return data
+        user = (
+            db.session.query(RegistryAdmissao)
+            .select_from(Funcionarios)
+            .join(RegistryAdmissao.funcionario)
+            .filter(Funcionarios.id == data["id"])
+            .all()
+        )  # noqa: F841
+
+        dataReturn = [  # noqa: N806
+            RegistroAdmissao(
+                data_solicitacao=item.data,
+                prazo=item.prazo,
+                solicitacao_desc="Formulário Admissional",
+            )
+            for item in user
+        ]
+
+        return dataReturn
+        # path_parent = Path(__file__).cwd().joinpath("dusdoc_api", "examples", "example_solicitados.json")
+        # async with aiofiles.open(str(path_parent), "r", encoding="utf-8") as f:
+        #     readfile = await f.read()
+        #     data = json.loads(readfile)
+        #     return data
